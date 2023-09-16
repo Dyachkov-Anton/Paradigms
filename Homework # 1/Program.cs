3af4b1adc2456d36ba5d8c5769e846a5b6bfd3b9
Dyachkov Anton <dyachkov1984@inbox.ru>
2023-09-16 13:05:22 +0300

    Homework 1

---
+++ Homework # 1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using _01_sorts;
+
+Console.Clear();
+
+Sorts sorts = new Sorts();
+
+int[] arrFirst = { 8, 0, -4, 7, -2, 2, -9, 3 };
+int[] arrSecond = { 80, 0, -40, 70, -20, 20, -90, 30 };
+
+Console.WriteLine($"Исходный массив для метода SortImperative: \t\t[ {string.Join(", ", arrFirst)} ]");
+Console.WriteLine($"Исходный массив для метода SortDeclarative: \t\t[ {string.Join(", ", arrSecond)} ]");
+
+sorts.SortImperative(arrFirst);
+Console.WriteLine($"Отсортированный массив методом SortImperative: \t\t[ {string.Join(", ", arrFirst)} ]");
+
+sorts.SortDeclarative(arrSecond);
+Console.WriteLine($"Отсортированный массив методом SortDeclarative: \t[ {string.Join(", ", arrSecond)} ]");

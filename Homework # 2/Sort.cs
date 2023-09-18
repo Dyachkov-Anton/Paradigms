7f6b4bf51bd683b988fd5be0dcec14fbf83d8573
Dyachkov Anton <dyachkov1984@inbox.ru>
2023-09-18 19:54:23 +0300

    Homework 2

---
+++ Homework # 2/Sort.cs
@@ -0,0 +1,24 @@
+﻿namespace _02_Sort
+{
+  internal class Sort
+  {
+    public Sort() { }
+
+    public int[] BubbleSort(int[] array)
+    {
+      for (int i = 0; i < array.Length; i++)
+      {
+        for (int j = 0; j < array.Length - i - 1; j++)
+        {
+          if (array[j] > array[j + 1])
+          {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+          }
+        }
+      }
+      return array;
+    }
+  }
+}
